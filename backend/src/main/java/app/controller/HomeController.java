a771f7b62a5a47693c5f511c8822c4cb3d685b69
DESKTOP-RDQC472\Duc <alaeena00@gmail.com>
2025-07-29 23:35:57 +0700

    [IMP] add deployment for github page

--- backend/src/main/java/app/controller/HomeController.java
+++ backend/src/main/java/app/controller/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String index() {
-        return "updated";
+        return "[IMP] add deployment for github page";
     }
 }
